4947f6370bd041ddd8b931eb757ef2b65aee3500
octo <allblvnk@gmail.com>
2021-01-25 23:10:59 +0000

    Created target pool.

--- Assets/TargetPreview/Scripts/TargetPool.cs
+++ Assets/TargetPreview/Scripts/TargetPool.cs
@@ -1,8 +1,55 @@
-using System.Collections;
 using System.Collections.Generic;
+using TargetPreview.Models;
 using UnityEngine;
 
-public class TargetPool : MonoBehaviour
+namespace TargetPreview.Display
 {
+    public class TargetPool : MonoBehaviour
+    {
+        const int poolSize = 200;
+        Target targetPrefab;
+        Stack<Target> targetPool = new Stack<Target>();
+
+        void Awake() => 
+            FillPool(poolSize);
+        
+        void FillPool(int size)
+        {
+            for (int i = 0; i < poolSize; i++)
+                CreateTarget();
+        }
+
+        void CreateTarget()
+        {
+            Target allocatedTarget = Instantiate(targetPrefab, transform);
+            allocatedTarget.TargetData = new TargetData(placeHolder: true);
+            allocatedTarget.gameObject.SetActive(false);
+            targetPool.Push(allocatedTarget);
+        }
+
+        /// <summary>
+        /// Takes a target from the <see cref="TargetPool"/>.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns>A reference to the target</returns>
+        public Target Take(TargetData data)
+        {
+            Target storedTarget = targetPool.Pop();
+            storedTarget
+                .TargetData = data;
+            storedTarget
+                .transform.SetParent(null); // Unparented objects perform better,
+            storedTarget                    // but it might be unnecessary.
+                .gameObject.SetActive(true);
+            return storedTarget;
+        }
+
+        public void Return(Target target)
+        {
+            target.gameObject.SetActive(false);
+            target.transform.SetParent(transform);
+            targetPool.Push(target);
+        }
+    }
 
 }
