cb2d7c42629acde1dc633de6ce7b71560fd39eab
octo <allblvnk@gmail.com>
2021-01-26 04:25:29 +0000

    Updated main scene, added TargetTransform

---
+++ Assets/TargetPreview/Scripts/TargetTransform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TargetPreview.Models;
+
+namespace TargetPreview.Math
+{
+    public static class TargetTransform
+    {
+        public static TargetPosition CalculateTargetTransform(int pitch, (float, float, float) offset)
+        {
+            return new TargetPosition();
+        }
+    }
+}
