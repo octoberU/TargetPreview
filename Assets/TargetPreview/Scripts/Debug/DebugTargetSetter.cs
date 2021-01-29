9da120a0125f5bc420801376b853832abd39ccc5
octo <allblvnk@gmail.com>
2021-01-29 01:31:06 +0000

    Created a class for debugging target states

---
+++ Assets/TargetPreview/Scripts/Debug/DebugTargetSetter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TargetPreview.Models;
+using UnityEngine;
+
+public class DebugTargetSetter : MonoBehaviour
+{
+    [ContextMenu("SwitchTargetColor")]
+    void SwitchTargetColor()
+    {
+        var target = FindObjectOfType<Target>();
+        var targetData = target.TargetData;
+        var newHandType = targetData.handType == TargetHandType.Left ? TargetHandType.Right : TargetHandType.Left;
+        target.TargetData = new TargetData
+            (targetData.behavior, newHandType, targetData.time, targetData.transformData);
+    }
+
+    [ContextMenu("CycleBehavior")]
+    void CycleBehavior()
+    {
+        var target = FindObjectOfType<Target>();
+        var targetData = target.TargetData;
+        var newBehavior = (int)targetData.behavior == 3 ? TargetBehavior.Standard : (TargetBehavior)((int)targetData.behavior + 1);
+        target.TargetData = new TargetData
+            (newBehavior, targetData.handType, targetData.time, targetData.transformData);
+    }
+}
