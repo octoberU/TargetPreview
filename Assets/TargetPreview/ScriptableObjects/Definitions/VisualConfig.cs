97129140241a01138dca22b05c6d2005b9494014
octo <allblvnk@gmail.com>
2021-01-29 01:31:32 +0000

    Updated visual config with bloom amount and telegraph colors

--- Assets/TargetPreview/ScriptableObjects/Definitions/VisualConfig.cs
+++ Assets/TargetPreview/ScriptableObjects/Definitions/VisualConfig.cs
@@ -8,13 +8,23 @@ namespace TargetPreview.ScriptableObjects
         [Header("Colors")]
         public Color leftHandColor;
         public Color rightHandColor;
+        
+        [Header("Rendering")]
+        public float targetBloomAmount = 2.416924f;
 
         public static Color GetColorForHandType(TargetHandType handType) =>
             handType switch
             {
                 TargetHandType.Left => Instance.leftHandColor,
-                TargetHandType.Right => Instance.leftHandColor,
+                TargetHandType.Right => Instance.rightHandColor,
                 _ => Color.white,
             };
+
+        public static Color GetTelegraphColorForHandType(TargetHandType handType)
+        {
+            float factor = Mathf.Pow(2, Instance.targetBloomAmount);
+            Color handColor = GetColorForHandType(handType);
+            return new Color(handColor.r * factor, handColor.g * factor, handColor.b * factor);
+        }
     }
 }
